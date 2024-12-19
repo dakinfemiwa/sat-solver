0ddf5f602e80938e25a0e20e37fd81cf2db65ee4
dakinfemiwa <daveak1602@gmail.com>
2024-12-19 14:17:09 +0000

    New header file for structures used in algorithm.

---
+++ include/satsolve.h
@@ -0,0 +1,52 @@
+/*
+Contains structures used in SAT solving algorithm
+*/
+
+#include <memory>
+#include <vector>
+#include "formula.h"
+#include <unordered_map>
+
+enum formula_state {SAT, UNSAT, UNKNOWN};
+
+struct satisfiedClause {
+    bool satisfied;
+    shared_ptr<Formula> satisfyingLiteral;
+};
+
+struct status {
+    unordered_map<shared_ptr<Formula>, bool> valuations;
+    unordered_map<shared_ptr<Formula>, bool> wrongValuations;
+    vector<shared_ptr<Formula>> prevConjuncts;
+    vector<shared_ptr<Formula>> conjuncts;
+};
+
+struct conjunctStatus {
+    shared_ptr<Formula> conjunct;
+    //Stored whether a clause is satisfied and what literal satisfied it
+    satisfiedClause satisfiedClause;
+    //Data to identify at what stage a conjunct was satisfied
+    //  so if there needs to be backtracking that can be done
+    int level;
+    //
+    int conjunctCount;
+};
+
+//Checks if a literal has been removed from a clause
+struct literalStatus {
+    shared_ptr<Formula> literal;
+    bool removed;
+};
+
+struct satSolveStatus {
+    unordered_map<shared_ptr<Formula>, bool> valuations;
+    unordered_map<shared_ptr<Formula>, bool> wrongValuations;
+    vector<shared_ptr<struct conjunctStatus>> conjuncts;
+    int clausesLeft;
+    formula_state state;
+};
+
+//Functoins
+shared_ptr<struct satSolveStatus> satSolveInit(vector<shared_ptr<Formula>> conjuncts);
+
+
